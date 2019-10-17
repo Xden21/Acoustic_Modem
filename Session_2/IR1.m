40e2057b90cbc82f336588642d3f77cee0ea78ce
ThomasWillemot <1998thomas@hotmail.be>
2019-10-17 11:53:33 +0200

    no message

--- Session_2/IR1.m
+++ Session_2/IR1.m
@@ -1,10 +1,18 @@
 %Calculate impuls response.
 
 fs = 16000;
-sig = [1,zeros(1,5*fs)]; %impulse
+sig = [1,zeros(1,fs)]; %impulse
 dftsize = 256;
 [simin,nbsecs,fs] = initparams(sig,fs);
 
 sim('recplay');
-sigout = simout.signals.values; %Noise and signal together.
+sigout = simout.signals.values;
+[~,startVal] = max(sigout);
+endVal = startVal + 250;
+subplot(2,1,1);
+plotSign = sigout(startVal-15:endVal);
+plot(plotSign);
+
+subplot(2,1,2);
+plot(abs(fft(plotSign,fs)));
 
