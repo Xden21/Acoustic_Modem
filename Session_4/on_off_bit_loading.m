a10f81e5752c3635772a0bc113eb9d21f7e234a4
Dennis Debree <dennis.debree@student.kuleuven.be>
2019-11-05 13:57:10 +0100

    Fix for searching bins to disable in on off bit loading

--- Session_4/on_off_bit_loading.m
+++ Session_4/on_off_bit_loading.m
@@ -6,20 +6,24 @@ function ofdm_packet = on_off_bit_loading(sig, channel_model,qam_order, channel_
     
     % Determine which frequency bins we want to use
     sorted_coef = sort(channel_model, 'descend');
-    ind = ceil(length(sorted_coef)*channel_fraction);
+    
+    %only half of the fraction must be deleted, because their conjugate position will be deleted too
+    ind = ceil(length(sorted_coef)*channel_fraction/2);
 
     min_val = sorted_coef(ind);
 
-    
-    for i=1:length(channel_model)
-        if channel_model(i) < min_val
+    for i=1:length(channel_model)/2
+        % check both current bin and conjugate bin
+        if channel_model(i) < min_val || channel_model(end-i+1) < min_val
             channel_model(i) = 0;
+            % Mirror on conjugate
+            channel_model(end-i+1) = 0;
         else
             channel_model(i) = qam_order;
+            % Mirror on conjugate
+            channel_model(end-i+1) = qam_order;
         end
     end
-    qam_orders = [channel_model,0,fliplr(channel_model)];
     ofdm_packet = build_ofdm_packet(sig,qam_orders);
-    % Build packet so that the non used frequency bins are zero.
-    
+    % Build packet so that the non used frequency bins are zero.    
 end
