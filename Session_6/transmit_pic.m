10c6f5a3b98d0676daca63bbc4dca56383f3c8f7
Dennis Debree <dennis.debree@student.kuleuven.be>
2019-12-02 14:13:54 +0100

    Milestone 3

--- Session_6/transmit_pic.m
+++ Session_6/transmit_pic.m
@@ -5,16 +5,50 @@ prefix_length =150;
 Lt = 6; %amount of training frames
 Ld = 30; %amount of data frames
 fs = 16000;
-channel_order = 120;
+channel_order = 60;
+
+BWUsage = 100;
+
+%% Channel Estimation
+%random bitstream
 trainbits = randi([0 1],1,qam_dim*(nfft/2-1));
 
-%qam modulated training block
+%qam modulated tr   aining block
 trainblock = qam_mod(trainbits,qam_dim);
+
+%make a sequence of 100 trainingblocks
+ofdm_train_seq = repmat(trainbits,1,100);
+%ofdm of train seq
+qam_orders = no_bit_loading(nfft, qam_dim);
+
+Tx = ofdm_mod_est(ofdm_train_seq,qam_orders,prefix_length);
+[simin,nbsecs,fs,sync_pulse] = initparams(Tx,fs,channel_order);
+%sigout = fftfilt(h(1:channel_order),simin(:,1));
+sim('recplay');
+sigout = simout.signals.values;
+Rx =alignIO(sigout,sync_pulse,channel_order);
+[received,calc_channel_freq_resp] = ofdm_demod_est(Rx,qam_orders,prefix_length,trainblock);
+
+channel_est_err = ber(trainbits, received')
+%% Calculate qam orders
+
+qam_orders = on_off_bit_loading(calc_channel_freq_resp, qam_dim,1- BWUsage/100);
+
+%% Send picture with bitloading
+
+bitcount = 0;
+for i=1:nfft/2
+    bitcount = bitcount + qam_orders(i);
+end
+trainbits = randi([0 1],1,bitcount);
+
+%qam modulated training block
+%trainblock = qam_mod(trainbits,qam_dim);
 
 %get image data.
 [bitStream, imageData, colorMap, imageSize, bitsPerPixel] = imagetobitstream('image.bmp');
-qamStream = qam_mod(bitStream, qam_dim);
-[ofdmStream, amount_of_packs] = ofdm_mod(qamStream,trainblock,nfft, prefix_length,Lt,Ld);
+%qamStream = qam_mod(bitStream, qam_dim);
+[ofdmStream, trainblock,amount_of_packs] = ofdm_mod_bl(bitStream,qam_orders,prefix_length,trainbits,Lt,Ld);
 
 %test for BER 0;
 [simin,nbsecs,fs,sync_pulse] = initparams(ofdmStream,fs,channel_order);
@@ -23,8 +57,7 @@ qamStream = qam_mod(bitStream, qam_dim);
 sim('recplay');
 sigout = simout.signals.values;
 Rx =alignIO(sigout,sync_pulse,channel_order);
-Rx =Rx(1:length(ofdmStream));
-[output_sig,calc_channel_freq_resp] = ofdm_demod(Rx,nfft,prefix_length,trainblock,Lt,Ld);
-[~,am_of_pl] = size(calc_channel_freq_resp);
-received = qam_demod(output_sig, qam_dim);
+Rx =Rx(1:length(ofdmStream)+50);
+[output_sig,calc_channel_freq_resp] = ofdm_demod_bl(Rx,qam_orders,prefix_length,trainblock,Lt,Ld);
+received = output_sig;
 ber(bitStream',received)
